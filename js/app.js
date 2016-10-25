e23b24d6dcd3155b46d492a7e43401d5884eeba2
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-25 14:10:52 +0200

    Add the display on selected articles
    stocking md files on express server and serving them at corresping
    article click

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 (function(){
 	"use strict";
 	var app = {
+
+		selectorMD: $("#md"),
+		selectorArticles: $("#articles"),
+
 		init:function(){
-			// let's go
+			app.requestAjax();
+			app.listeners();
+		},
+
+		listeners: function(){
+			$("#articles").on("click", "a", function(){
+				app.displayMd($(this).data("url"));
+			});
+		},
+
+		requestAjax: function(){
+			var jsonRequest = $.ajax("http://192.168.1.107:8080/menu.json")
+			.done(function(){
+				app.getArticles(jsonRequest.responseJSON.menu);
+			})
+			.fail(console.log("fail"));
+		},
+
+		getArticles: function(menu){
+			var len = menu.length;
+			for(var i = 0; i < len; i++){
+				var url = "http://192.168.1.107:8080" + menu[i].path;
+				var title = menu[i].title;
+				var anchorArticle = "<a class='item' data-url='" + url + "'>" + title + "</a>";
+				app.selectorArticles.append(anchorArticle);
+			}
+		},
+
+		displayMd: function(url){
+			var converter = new showdown.Converter();
+			var mdRequest = $.ajax(url)
+			.done(function(){
+				var convertedToHtml = converter.makeHtml(mdRequest.responseText);
+				app.selectorMD.html(convertedToHtml);
+			})
+			.fail(console.log("fail displayMd")); //demander pk tjrs vrai
 		},
 	};
-
 
 	$(document).ready(function(){
 		app.init();
