c6810e873314bd4618f516bd9229247cca04a9f1
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-28 08:07:40 +0200

    Add the edit page integration

--- express_server/server.js
+++ express_server/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json();
+var textParser = bodyParser.text();
 
 var app = express();
+
 app.use(express.static('ressources'));
+
 		
 /*
 app.use(function(req, res, next){
@@ -14,9 +20,28 @@ app.use(function(err, req, res, next) {
   res.status(500).send('Something broke!');
 });*/
 
-app.use("/",function (req, res, next) {
-  console.log(req, "result");
+app.post('/',function (req, res) {
+	/*if (!req.body) {return res.sendStatus(400);}*/
+	console.log(req,req.body);
+	res.send("test");
+
+});
+/*
+app.use('/menu.json',function (req, res, next) {
+  console.log(req);
+  next();
+});*/
+
+app.use('/UUUUUUUU',function (req, res, next) {
+  console.log( textParser(req),"middle");
   next();
 });
+
+app.use('/',function (req, res, next) {
+  console.log(req);
+  next();
+});
+
+
 
 app.listen(8080);
--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 	"use strict";
 	var app = {
 
+		localurl: "http://192.168.43.133:8080",
 		selectorMD: $("#md"),
 		selectorArticles: $("#articles"),
 
@@ -11,14 +12,33 @@
 		},
 
 		listeners: function(){
+			var me = this;
 			$("#articles").on("click", "a", function(){
-				app.displayMd($(this).data("url"));
+				if($(this).data("type") === "edit"){
+					app.display($(this).data("url"), false);		
+				}else{
+					app.display($(this).data("url"), true);
+				}
+			});
+			$("#md").on("click", "#form-sub", function(event){
+				event.preventDefault();
+				console.log("click sub");
+				var title = $("#title").val();
+				$.ajax({
+					method: "POST",
+					url: me.localurl + "/" + title,
+					data: { TRRRUUUUUC : "bidule"},
+					success: function(){me.greatSuccess},
+					/*dataType: "text",*/
+				});
+
+				console.log(title);
 			});
 		},
 
 		requestJson: function(){
 			var me = this;	
-			var jsonRequest = $.ajax("http://192.168.1.107:8080/menu.json")
+			var jsonRequest = $.ajax(this.localurl + "/menu.json")
 			.done(function(){
 				me.getArticles(jsonRequest.responseJSON.menu);
 			})
@@ -28,24 +48,35 @@
 		getArticles: function(menu){
 			var len = menu.length;
 			for(var i = 0; i < len; i++){
-				var url = "http://192.168.1.107:8080" + menu[i].path;
+				var url = this.localurl + menu[i].path;
 				var title = menu[i].title;
 				var anchorArticle = "<a class='item' data-url='" + url + "'>" + title + "</a>";
 				app.selectorArticles.append(anchorArticle);
 			}
 		},
 
-		displayMd: function(url){
+		display: function(url, isMarkdown){
 			var converter = new showdown.Converter();
 			var mdRequest = $.ajax(url)
 			.done(function(){
-				var convertedToHtml = converter.makeHtml(mdRequest.responseText);
-				app.selectorMD.html(convertedToHtml);
+				if(isMarkdown === true){
+					var convertedToHtml = converter.makeHtml(mdRequest.responseText);
+					app.selectorMD.html(convertedToHtml);
+				}else{
+					app.selectorMD.html(mdRequest.responseText);
+				}
 			})
 			.fail(app.errorAjax);
 		},
 
 		errorAjax: function(){console.log("In fail method, something went wrong");},
+
+		greatSuccess: function(){
+			console.log("greatSuccess")
+		},
+		formClicked: function(){
+
+		},
 	};
 
 	$(document).ready(function(){
